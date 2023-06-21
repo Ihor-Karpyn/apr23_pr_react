2596e89be316207e8810b88d01ba596c53aab359
Stepan Haida <stepanhaida@gmail.com>
2023-06-21 10:38:37 +0300

    solution for task 1

--- src/App.jsx
+++ src/App.jsx
@@ -230,38 +230,6 @@ export const App = () => (
                 </tr>
               );
             })}
-
-            <tr data-cy="Product">
-              <td className="has-text-weight-bold" data-cy="ProductId">
-                2
-              </td>
-
-              <td data-cy="ProductName">Bread</td>
-              <td data-cy="ProductCategory">🍞 - Grocery</td>
-
-              <td
-                data-cy="ProductUser"
-                className="has-text-danger"
-              >
-                Anna
-              </td>
-            </tr>
-
-            <tr data-cy="Product">
-              <td className="has-text-weight-bold" data-cy="ProductId">
-                3
-              </td>
-
-              <td data-cy="ProductName">iPhone</td>
-              <td data-cy="ProductCategory">💻 - Electronics</td>
-
-              <td
-                data-cy="ProductUser"
-                className="has-text-link"
-              >
-                Roma
-              </td>
-            </tr>
           </tbody>
         </table>
       </div>
